7ab734f734b5d6cc422c05907984659c56f32cd4
yousefhussen <yousefhussen139@gmail.com>
2025-02-27 13:23:56 +0200

    auth not modular

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use App\UserResource;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+use function Pest\Laravel\json;
+
+class RedirectIfAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$guards): \Illuminate\Http\JsonResponse
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                return \response()->json(['message' => 'Already authenticated','user'=> UserResource::make(Auth::user())], Response::HTTP_ACCEPTED);
+            }
+        }
+
+        return $next($request);
+    }
+}
